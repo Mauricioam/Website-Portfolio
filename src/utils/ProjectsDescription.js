2c4a7cdb2054a34ed4d6cedd0b8e86b901bcc4e8
Mauricioam <mauricioam91@gmail.com>
2023-02-10 14:56:56 -0300

    changing title and adding a logo

--- src/utils/ProjectsDescription.js
+++ src/utils/ProjectsDescription.js
@@ -1,21 +1,22 @@
 import projectImageWeather from "../assets/Weather-app.jpg";
 import projectImageAlkeFlix from "../assets/AlkeFlix.jpg";
-import projectImageHenryDogs from "../assets/henryDogs-image.jpg"
+import projectImageHenryDogs from "../assets/henryDogs-image.jpg";
+import projectImageTechStore from "../assets/techStore-image.jpg";
 
 export const projectsDescription = [
     {
+        title:"TechStore E-commerce",
+        href:"https://www.loom.com/share/9466c30fc07b4d728c17d1787b0cd6e4",
+        description:"Final project for Full Stack developer degree at Henry Bootcamp. In this project I worked with a team of 6 developers and a SCRUM master having a daily progress report. I took on a Frontend role working on the designs and dynamics of various pages. After being approved we are currently changing the styles refactoring code, and adding new features but soon the deploy will be available. ",
+        github:"https://github.com/Mauricioam/WeatherApp2.0",
+        image:projectImageTechStore   
+        
+    },{
         title: "Henry Dogs",
         href:"https://www.loom.com/share/7ed24f7a6a86491f8ae8b35ed8b73dd3",
         description:"Challenge I developed during Henry Bootcamp course. In this project I assume a Full Stack role developing the back-end with Node.js and Express, the database with PostgreSQL and Sequalize, and in the Front using React.js and Redux to make everything dynamic.",
         github:"https://github.com/Mauricioam/ProyectoHenryDogs",
         image:projectImageHenryDogs
-        
-    },{
-        title: "AlkeFlix",
-        href:"https://alkemy-challenge-tau.vercel.app/",
-        description:"Challenge I developed during the Skill Up React course at Alkemy.org. The main features are the search for movies by title and you can add to a favorites section. Credentials for demo email: challenge@alkemy.org, password: react",
-        github:"https://github.com/Mauricioam/AlkemyChallenge",
-        image:projectImageAlkeFlix
     },{
         
         title:"Weather Web",
@@ -23,11 +24,11 @@ export const projectsDescription = [
         description:"Spa developed with React.js and Bootstrap. I wanted to challenge myself to made a responsive weather web with a forecast for the next 24hs.I use the Weather API from OpenWeatherMap",
         github:"https://github.com/Mauricioam/WeatherApp2.0",
         image:projectImageWeather   
-},
-{
-    title:"TechStore E-commerce",
-    href:"https://weather-app2-0-mauricioam.vercel.app/",
-    description:"Spa developed with React.js and Bootstrap. I wanted to challenge myself to made a responsive weather web with a forecast for the next 24hs.I use the Weather API from OpenWeatherMap",
-    github:"https://github.com/Mauricioam/WeatherApp2.0",
-    image:projectImageWeather   
+    },
+    {
+        title: "AlkeFlix",
+        href:"https://alkemy-challenge-tau.vercel.app/",
+        description:"Challenge I developed during the Skill Up React course at Alkemy.org. The main features are the search for movies by title and you can add to a favorites section. Credentials for demo email: challenge@alkemy.org, password: react",
+        github:"https://github.com/Mauricioam/AlkemyChallenge",
+        image:projectImageAlkeFlix
 }]
--- src/utils/aboutMe.js
+++ src/utils/aboutMe.js
@@ -1,4 +1,4 @@
 export const aboutMe = {
     title: "Get to know me",
-    description : "I'm a Frontend Web Developer focused on building dynamic websites. I'm very disciplined  and goal-oriented to every project objective. Also I' really like working with teammates and I'm always available for pair programming. I'm open to job opportunities where I can contribute, but most important to me, to learn and grow as a developer. I'm glad you end up in my website so you can check my projects and skills. If you have a Job opportunity thath matches my skills don't hesitate to contact me."
+    description : "I'm a Frontend Web Developer focused on building dynamic websites. I'm very disciplined  and goal-oriented to every project objective. Also I really like working with teammates and I'm always available for pair programming. I'm open to job opportunities where I can contribute, but most important to me, to learn and grow as a developer. I'm glad you end up in my website so you can check my projects and skills. If you have a Job opportunity thath matches my skills don't hesitate to contact me."
 }
