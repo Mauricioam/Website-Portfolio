2cb8042d397ee004681e4cfd3e5fa732de62d878
Mauricioam <mauricioam91@gmail.com>
2023-03-01 11:39:45 -0300

    change cv link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   const [menu, setMenu] = useState(false);
 
     const TitlesWideScreenStyle = " text-sky font-title text-lg font-bold text-md hover:text-text3 hover:transition-all";
-    const TitlesMobileStyles = `${TitlesWideScreenStyle} font-bold border  flex w-screen h-12 items-center justify-end pr-20 hover:text-primary transition-all`
+    const TitlesMobileStyles = `${TitlesWideScreenStyle} font-bold   w-full  pr-20 hover:text-primary `
     const menuItems = ["Home","Get to know me!","Projects"]
 
 
@@ -17,7 +17,7 @@ function Header() {
         <div className="flex ml-5 flex-1  gap-6">
           
         </div>
-         <button onClick={()=>setMenu(!menu)} type="button" className="flex mr-20 md:hidden">
+         <button onClick={()=>setMenu(!menu)} type="button" className="flex mr-20 md:hidden" data-te-dropdown-toggle-ref>
          {menu ? (<CloseIcon/>): (<MenuIcon/>)}
         </button> 
         
@@ -26,14 +26,14 @@ function Header() {
             {menuItems.map((item,i) =>(<li key={i} className={TitlesWideScreenStyle}><a href={`#${item}`}>{item}</a></li>))}
           </ul>
         </div>
-      </header>
       {/* Dropdown menu */}
-     {menu && (<div className={`md:invisible z-10 transition-all   w-screen shadow-2xl bg-sky  absolute border  justify-end `}>
-        <ul className="flex flex-col   text-right">
+     <div className={`md:hidden z-10  ${menu ? "h-40":"invisible"} h-0  transition-all duration-500  top-20   w-screen shadow-2xl bg-sky  absolute border  justify-end `}>
+        <ul className={`${menu ? "flex divide-y divide-solid translate-y-0 transition-transform duration-500 h-full ":"-translate-y-10 transition-transform duration-500 "} z-0 flex-col  gap-4 h-full text-right justify-center`}>
            {menuItems.map((item,i) =>(<li key={i} className={TitlesMobileStyles}><a className="transition-all text-dark1 hover:text-primary" onClick={()=>setMenu(!menu)} href={`#${item}`}>{item}</a></li>))}
         </ul>
-      </div>)} 
+      </div>
       {/* Dropdown menu */}
+      </header>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import AboutMe from '../components/AboutMe';
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
-import Projects from '../components/Projects';
-import Welcome from '../components/Welcome';
+import React from "react";
+import AboutMe from "../components/AboutMe";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Projects from "../components/Projects";
+import Welcome from "../components/Welcome";
 
 function Home() {
-
   return (
     <div>
-        <Header/>
-        <Welcome/> 
-        <AboutMe/> 
-        <Projects/> 
-         <Footer/>  
+      <Header />
+      <Welcome />
+      <AboutMe />
+      <Projects />
+      <Footer />
     </div>
-  )
-};
+  );
+}
 
 export default Home;
